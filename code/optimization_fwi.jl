ce362ab55877a9355b6fd9ac0798a2520b5292d4
zzar43 <lida7980@gmail.com>
2019-10-23 11:09:25 -0600

    opt

--- code/optimization_fwi.jl
+++ code/optimization_fwi.jl
@@ -215,12 +215,13 @@ function obj_fn_parallel(data0, c, rho, Nx, Ny, Nt, h, dt, source, source_positi
     return fk, gradk
 end
 
-function obj_fn_sinkhorn_parallel(data0, c, rho, Nx, Ny, Nt, h, dt, source, source_position, receiver_position; pml_len=10, pml_coef=100, lambda=1e2, numItermax=10, stopThr = 1e-6)
+function obj_fn_sinkhorn_parallel(data0, c, rho, Nx, Ny, Nt, h, dt, source, source_position, receiver_position; pml_len=10, pml_coef=100, reg=5e-3, reg_m=1e2, reg_p=0, iterMax=100, verbose=false)
     x = reshape(c, Nx, Ny)
 
     data, u = multi_solver_parallel(x, rho, Nx, Ny, Nt, h, dt, source, source_position, receiver_position; pml_len=pml_len, pml_coef=pml_coef)
 
-    gradk, fk = grad_sinkhorn_parallel(data, u, data0, x, rho, Nx, Ny, Nt, h, dt, source_position, receiver_position; pml_len=pml_len, pml_coef=pml_coef, lambda=lambda, numItermax=numItermax, stopThr=stopThr)
+#     gradk, fk = grad_sinkhorn_parallel(data, u, data0, x, rho, Nx, Ny, Nt, h, dt, source_position, receiver_position; pml_len=pml_len, pml_coef=pml_coef, lambda=lambda, numItermax=numItermax, stopThr=stopThr)
+    gradk, fk = grad_sinkhorn_parallel(data, u, data0, c, rho, Nx, Ny, Nt, h, dt, source_position, receiver_position; reg_p=reg_p, pml_len=pml_len, pml_coef=pml_coef, reg=reg, reg_m=reg_m, iterMax=iterMax, verbose=verbose)
 
     gradk = reshape(gradk, Nx*Ny, 1)
     return fk, gradk
